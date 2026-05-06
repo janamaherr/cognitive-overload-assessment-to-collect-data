2c3bb634beb03f23fceb1d167bf723bda5178982
janamaherr <jana.maher36@gmail.com>
2026-05-06 03:37:07 +0300

    baseline hr

--- obj/Debug/net9.0/CognitiveOverloadLMS.AssemblyInfo.cs
+++ obj/Debug/net9.0/CognitiveOverloadLMS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CognitiveOverloadLMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26a5999a603749d2f9f086695103c1266ba72988")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dace625ff0a71794cbc0e745fe0b3e9f03432d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("CognitiveOverloadLMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CognitiveOverloadLMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
